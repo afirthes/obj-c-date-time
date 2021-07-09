c6a57910c8fcf0d441431703dad96528bde5c556
afirthes <afirthes@gmail.com>
2021-07-09 20:57:10 +0300

    Initial Commit

---
+++ obj-c-date-time/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  obj-c-date-time
+//
+//  Created by sehio on 09.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ obj-c-date-time/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  obj-c-date-time
+//
+//  Created by sehio on 09.07.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
